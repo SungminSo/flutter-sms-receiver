4cb6f1ea0daba6787f16db7f03e2859381f5f35b
SungminSo <soos196@hanyang.ac.kr>
2020-07-19 17:57:39 +0900

    change stateful to stateless

--- lib/main.dart
+++ lib/main.dart
@@ -22,37 +22,16 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ReceiverPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ReceiverPage extends StatelessWidget {
   Slack slack = new Slack('https://hooks.slack.com/services/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');
   SmsReceiver receiver = new SmsReceiver();
 
-  void initState() {
-    super.initState();
-
+  _addListener() {
     receiver.onSmsReceived.listen((SmsMessage msg) {
       if (msg.sender == "01039167481" || msg.sender == "16449999") {
         List<String> textLines = msg.body.split("\n");
@@ -96,15 +75,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _sendTestMessage() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-
     Attachment cardReport = new Attachment("card detail report",
         pretext: "카드 사용 내역",
         text: '테스트 메세지입니다.',
@@ -123,45 +93,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    _addListener();
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Text(
+          "sms-receiver"
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _sendTestMessage,
-        tooltip: 'Increment',
+        tooltip: 'Test',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
